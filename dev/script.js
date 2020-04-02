6d97bc3b639040ab3c72ecfeb794ffc6b3f47145
Vítor Avelino <contact@vitoravelino.me>
2020-04-01 22:27:27 -0300

    mobile: disable particles
    Signed-off-by: Vítor Avelino <contact@vitoravelino.me>

--- dev/script.js
+++ dev/script.js
@@ -1,5 +1,10 @@
-import 'tsparticles';
+const isMobile = window.matchMedia('only screen and (max-width: 1279px)')
+  .matches;
 
-tsParticles.load('particles', {
-  preset: 'basic'
-});
+if (!isMobile) {
+  require('tsparticles');
+
+  tsParticles.load('particles', {
+    preset: 'basic',
+  });
+}
